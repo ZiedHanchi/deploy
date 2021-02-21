70361005e4518b84e134e2bab7b10db18f99cc68
ZiedHanchi <zied_hanchi1980@yahoo.fr>
2021-02-21 10:32:48 -0500

    unzip

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -8,4 +8,6 @@ chmod 600 ~/.kaggle/kaggle.json
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 
-unzip -o packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip packages/regression_model/regression_model/datasets/
+unzip -o packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip -d "packages/regression_model/regression_model/datasets/"
+
+rm packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
