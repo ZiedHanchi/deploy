ab3e9b5be9c3b74306a3582850340ff4520ce22b
ZiedHanchi <zied_hanchi1980@yahoo.fr>
2021-02-20 21:21:08 -0500

    kaggle

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+export PATH=$PATH:$(pwd)
+
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
