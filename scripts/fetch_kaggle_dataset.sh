f32a0ad7139a1da58d1dde7c12539e2836e8ccb6
ZiedHanchi <zied_hanchi1980@yahoo.fr>
2021-02-21 10:24:53 -0500

    unzip

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,10 +2,10 @@
 
 export PATH=$PATH:$(pwd)
  
-cp -f kaggle.json ~/.kaggle
+cp -f .kaggle/kaggle.json ~/.kaggle
 
 chmod 600 ~/.kaggle/kaggle.json
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 
-unzip -n packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip
+unzip -o packages/regression_model/regression_model/datasets/house-prices-advanced-regression-techniques.zip packages/regression_model/regression_model/datasets/
